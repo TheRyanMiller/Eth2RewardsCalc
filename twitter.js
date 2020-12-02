997d994d3b020a20f00207ebae056b579847b4df
Ryan Miller <rmiller07@gmail.com>
2020-12-02 17:36:39 -0500

    4x daily

--- twitter.js
+++ twitter.js
@@ -16,7 +16,7 @@ let client2 = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET2
 });
 
-let cronValue = "0 8,20 * * *";
+let cronValue = "0 0,6,12,18 * * *";
 
 const test = () => {
     eth2calc().then(data=>{
