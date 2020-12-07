6b85bbe18d558fc9f108bb100a6d8c76443c6870
Ryan Miller <rmiller07@gmail.com>
2020-12-07 14:46:06 -0500

    update to once per day

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-let hexString = "8032136ccef5c74f27339838c5b14b498752dafc16c425dd77a288229931b6ad6c306e4bb26135e6137cd5c694a3b729";
-let hex2 ="943fcbb4a086f71960ddffcdcf25b3b350738cb53c871566f8bde93a24c0d01ee987b3e5a444d934c2e5cf818925804e"
+let hexString = "example pubkey string";
+let hex2 ="example pubkey string"
 base64String = Buffer.from(hex2, 'hex').toString('base64');
 let encoded = encodeURIComponent(base64String)
 console.log(encoded);
